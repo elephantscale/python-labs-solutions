8990d46f5604f7d3502d7396e2b396f9cb281dc5
Sujee Maniyam <sujee@elephantscale.com>
2020-01-21 14:11:49 -0600

    data cleanup solution update

--- package-labs.sh
+++ package-labs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-$ES_HOME/utils/labs/package-labs.sh $@
+
+$ES_HOME/utils/labs/labs-assembler.sh  lab-assembly.txt  $@
+
+# link data dir for easy testing
+this_dir=$(basename `pwd`)
+output_dir=${1:-$this_dir}
+(cd "labs.out/$output_dir" ; ln -s ../../data  ./data )
+
